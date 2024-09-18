a3be7e5e4ce0059cc3d7da8983e97d964cf4a360
Youssef Reda <39743115+youssefreda02@users.noreply.github.com>
2024-09-19 00:31:43 +0300

    Upload run.py

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:////D:/New folder/DS NanoDegree files/Disaster Response Pipeline/data/DisasterResponse.db')
+engine = create_engine('sqlite:///.../data/DisasterResponse.db')
 df = pd.read_sql_table('message_category', engine)
 
 # load model
-model = joblib.load("/D:/New folder/DS NanoDegree files/Disaster Response Pipeline/models/classifier.pkl")
+model = joblib.load("/.../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
