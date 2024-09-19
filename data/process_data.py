594824e966360c7705ab69ec970bee3587797eec
Youssef Reda <39743115+youssefreda02@users.noreply.github.com>
2024-09-19 03:04:29 +0300

    Bug fixed process_data.py
    if_exist = 'replace' added to solve overwriting error while saving DataFrame into SQL table.

--- data/process_data.py
+++ data/process_data.py
@@ -50,6 +50,7 @@ def clean_data(df):
 def save_data(df, database_filename):
     engine = create_engine(f'sqlite:///{database_filename}')
     df.to_sql('message_category', engine, index=False)
+    df.to_sql('message_category', engine, index=False, if_exists= 'replace')
     pass  
 
 
